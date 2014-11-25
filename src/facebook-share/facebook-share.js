61c9fd36ddeddc8ed9c21a378410cf3b89d5690e
Petri Koivula <pkjedi@iki.fi>
2014-11-25 20:28:21 +0200

    fb url fix

--- src/facebook-share/facebook-share.js
+++ src/facebook-share/facebook-share.js
@@ -9,7 +9,7 @@ define([], function () {
                 link: function (scope, element) {
                     element[0].children[0].src =
                         '//www.facebook.com/plugins/share_button.php?href=' +
-                        encodeURIComponent(window.location) +
+                        window.location +
                         '&layout=button_count&appId=1472730843000242';
                 }
             };
